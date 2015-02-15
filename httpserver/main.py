acacb47cd466de1ec65ea61b07654f902ac7db1a
teddy-hoo <hulingchuan@hotmail.com>
2015-02-15 12:49:27 +0800

    add main function

---
+++ httpserver/main.py
@@ -0,0 +1,32 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', ''))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'helpers'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'models'))
+
+from retrieve import *
+
+URL = ''
+
+# retrieve comments from jingdong
+retrieve([URL])
+
+# segment the comments
+
+
+# tag part of speach for the comments
+
+
+# remove disturb comments
+
+
+# extract chunk from comments
+
+
+# extract feed words
+
+
+# extract feature and description
+
+
+# extract ue evaluation
+
